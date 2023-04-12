3135e04ea65cb8b1c6ad26f35f70532cb759a09f
daniloeberli <danilo.eberli@gmail.com>
2023-04-12 17:18:33 +0200

    feat: add task ( bugged)

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ createApp({
     data() {
         return {
             tasks: [],
+            newTask: ''
         }
     },
     methods: {
- 
+        addTask(){
+            //console.log('cliccato');
+            const data = {
+                newtask: this.newTask
+            };
+            axios.post('server.php', data, {
+                header: { 'Content-Type': 'multipart/form-data'}
+            })
+
+            .then((response) => {
+                this.tasks = response.data;
+                this.newTask = '';
+            })
+        }
     },
     created() {
         axios.get('server.php')
--- server.php
+++ server.php
@@ -1,8 +1,19 @@
 <?php
 
 $str = file_get_contents(__DIR__ . '/task.json');
-
 $tasks = json_decode($str, true);
+
+
+if(isset($_POST['newTask'])){
+    $new_task = $_POST['newTask'];
+
+    $todo[] = [
+        'text'=> $new_task,
+        'done'=>false
+    ]; 
+
+    file_put_contents(__DIR__ . '/task.json', json_encode($todo));
+}
 
 
 header("Content-Type: application/json");
